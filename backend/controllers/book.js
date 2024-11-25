f7b37527a4076ab895cd90f14f0d9e916e45aeb0
SophiaSophia03 <sophiasophia1224@gmail.com>
2024-11-25 13:07:49 -0600

    Created Book routes

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -3,52 +3,105 @@ const Book = require("../models/book.js");
 const asyncHandler = require("express-async-handler");
 
 // add-books *admin
-module.exports.addBooks = asyncHandler(async(req,res) => {
+module.exports.addBooks = asyncHandler(async (req, res) => {
   try {
     const { id } = req.headers;
     const user = await User.findById(id);
-    if(user.role !== "admin"){
-      return res.status(400).json({message:"YOu are not allowed to perform this task"})
+    if (user.role !== "admin") {
+      return res
+        .status(400)
+        .json({ message: "YOu are not allowed to perform this task" });
     }
 
-  const book = new Book({
-    url:req.body.url,
-    title:req.body.title,
-    author:req.body.author,
-    price:req.body.price,
-    desc:req.body.desc,
-    language:req.body.language,
-  })
-  await book.save();
-  return res.status(200).json({message:"Book added successfuly!"})
-  } catch(err){
-    console.log(err)
-    // res.status(500).json({message:"Internal Server Error"});
+    const book = new Book({
+      url: req.body.url,
+      title: req.body.title,
+      author: req.body.author,
+      price: req.body.price,
+      desc: req.body.desc,
+      language: req.body.language,
+    });
+    await book.save();
+    return res.status(200).json({ message: "Book added successfuly!" });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
   }
-})
+});
 
+//update-books *admin
+module.exports.updateBook = asyncHandler(async (req, res) => {
+  try {
+    const { bookid, id } = req.headers;
+    const user = await User.findById(id);
+    if (user.role !== "admin") {
+      return res
+        .status(400)
+        .json({ message: "YOu are not allowed to perform this task" });
+    }
+    await Book.findByIdAndUpdate(bookid, {
+      url: req.body.url,
+      title: req.body.title,
+      author: req.body.author,
+      price: req.body.price,
+      desc: req.body.desc,
+      language: req.body.language,
+    });
+    return res.status(200).json({ message: "Book updated successfuly!" });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
+//delete-books *admin
+module.exports.deleteBook = asyncHandler(async (req, res) => {
+  try {
+    const { bookid, id } = req.headers;
+    const user = await User.findById(id);
+    if (user.role !== "admin") {
+      return res
+        .status(400)
+        .json({ message: "YOu are not allowed to perform this task" });
+    }
+    await Book.findByIdAndDelete(bookid);
+    return res.status(200).json({ message: "Book deleted successfuly!" });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
+//get-all-books
+module.exports.getAllBooks = asyncHandler(async (req, res) => {
+  try {
+    const allBooks = await Book.find().sort({ createdAt: -1 });
+    return res
+      .status(200)
+      .json({ status: "Success", data: allBooks });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
+//get-recent-books
+module.exports.getRecentBooks = asyncHandler(async (req, res) => {
+  try {
+    const recBooks = await Book.find().sort({ createdAt: -1 }).limit(5);
+    return res
+      .status(200)
+      .json({ status: "Success", data: recBooks });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
-//Get-Books
-// module.exports.getBooks = asyncHandler(async(req,res) => {
-//   try {
-//     const { bookId } = req.headers;
-//     const data = await Book.findById(id);
-//     return res.status(200).json(data);
-//   } catch(err){
-//     res.status(500).json({message:"Internal Server Error"});
-//   }
-// })
-
-
-
-
-
-
-
-
-
-
-
+//get-book-details
+module.exports.bookDetails = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    const bookDetails = await Book.findById(id);
+    return res
+      .status(200)
+      .json({ status: "Success", data: bookDetails });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -5,4 +5,16 @@ const { authenticateToken} = require("../middlewares/authMiddleware.js");
 
 router.route("/addBooks").post(authenticateToken, BookController.addBooks);
 
+router.route("/updateBook").put(authenticateToken, BookController.updateBook);
+
+router.route("/deleteBook").delete(authenticateToken, BookController.deleteBook);
+
+router.route("/getBooks").get(BookController.getAllBooks);
+
+router.route("/recBooks").get(BookController.getRecentBooks);
+
+router.route("/bookDetails/:id").get(BookController.bookDetails);
+
+
+
 module.exports = router;
